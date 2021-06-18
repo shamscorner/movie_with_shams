3795268de4b151bdfdc93281583cff35cf9b67f9
Shamim Hossain <hossains159@gmail.com>
2021-06-19 01:19:44 +0600

    chore: add api client setup

---
+++ lib/data/core/api_constants.dart
@@ -0,0 +1,7 @@
+class ApiConstants {
+  ApiConstants._(); // private constructor
+
+  static const String BASE_URL = "https://api.themoviedb.org/3/";
+  static const String API_KEY = "c5c2f10d9cacb54a6a3732ff1ef0e628";
+  static const String BASE_IMAGE_URL = "https://image.tmdb.org/t/p/w500";
+}
