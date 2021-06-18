23502469510bd27f87f805fc19301cbae729a319
Shamim Hossain <hossains159@gmail.com>
2021-06-19 03:20:36 +0600

    refactor: add get it package to remove boilerplate initials

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,21 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'package:movie_with_shams/data/core/api_client.dart';
-import 'package:movie_with_shams/data/data_sources/movie_remote_data_source.dart';
-import 'package:movie_with_shams/data/repositories/movie_repository_impl.dart';
 import 'package:movie_with_shams/domain/entities/movie_entity.dart';
 import 'package:movie_with_shams/domain/entities/no_params.dart';
-import 'package:movie_with_shams/domain/repositories/movie_repository.dart';
 import 'package:movie_with_shams/domain/usecases/get_coming_soon.dart';
 import 'package:movie_with_shams/domain/usecases/get_playing_now.dart';
 import 'package:movie_with_shams/domain/usecases/get_popular.dart';
 import 'package:movie_with_shams/domain/usecases/get_trending.dart';
+import 'package:pedantic/pedantic.dart';
+import 'di/get_it.dart' as getIt;
 
 import 'domain/entities/app_error.dart';
 
 void main() async {
-  ApiClient apiClient = ApiClient(Client());
-  MovieRemoteDataSource dataSource = MovieRemoteDataSourceImpl(apiClient);
-  MovieRepository movieRepository = MovieRepositoryImpl(dataSource);
+  unawaited(getIt.init());
 
   // get trending movies
-  GetTrending getTrending = GetTrending(movieRepository);
+  GetTrending getTrending = getIt.getItInstance<GetTrending>();
   Either<AppError, List<MovieEntity>> eitherResponse =
       await getTrending(NoParams());
   eitherResponse.fold((l) {
@@ -32,7 +27,7 @@ void main() async {
   });
 
   // get popular movies
-  GetPopular getPopular = GetPopular(movieRepository);
+  GetPopular getPopular = getIt.getItInstance<GetPopular>();
   eitherResponse = await getPopular(NoParams());
   eitherResponse.fold((l) {
     print('error');
@@ -43,7 +38,7 @@ void main() async {
   });
 
   // get playing now movies
-  GetPlayingNow getPlayingNow = GetPlayingNow(movieRepository);
+  GetPlayingNow getPlayingNow = getIt.getItInstance<GetPlayingNow>();
   eitherResponse = await getPlayingNow(NoParams());
   eitherResponse.fold((l) {
     print('error');
@@ -54,7 +49,7 @@ void main() async {
   });
 
   // get coming soon movies
-  GetComingSoon getComingSoon = GetComingSoon(movieRepository);
+  GetComingSoon getComingSoon = getIt.getItInstance<GetComingSoon>();
   eitherResponse = await getComingSoon(NoParams());
   eitherResponse.fold((l) {
     print('error');
