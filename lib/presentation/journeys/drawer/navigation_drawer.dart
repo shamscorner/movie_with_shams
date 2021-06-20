27287638c0facb981d29979ee490e7f69bc40620
Shamim Hossain <hossains159@gmail.com>
2021-06-20 17:31:19 +0600

    chore: add navigation drawer widget

---
+++ lib/presentation/journeys/drawer/navigation_drawer.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:movie_with_shams/common/constants/size_constants.dart';
+import 'package:movie_with_shams/common/extensions/size_extensions.dart';
+import 'package:movie_with_shams/presentation/widgets/logo.dart';
+
+class NavigationDrawer extends StatelessWidget {
+  const NavigationDrawer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: Theme.of(context).primaryColor.withOpacity(0.7),
+            blurRadius: 4,
+          )
+        ],
+      ),
+      width: Sizes.dimen_250.w.toDouble(),
+      child: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: EdgeInsets.only(
+                top: Sizes.dimen_12.h.toDouble(),
+                bottom: Sizes.dimen_32.h.toDouble(),
+                left: Sizes.dimen_12.h.toDouble(),
+                right: Sizes.dimen_12.h.toDouble(),
+              ),
+              child: Logo(
+                height: Sizes.dimen_48.h.toDouble(),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
