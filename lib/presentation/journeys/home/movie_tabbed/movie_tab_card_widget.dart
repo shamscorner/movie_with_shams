0cac3ddc1d018ac7faf849c5f9f2d91a2b08b470
Shamim Hossain <hossains159@gmail.com>
2021-06-20 14:07:22 +0600

    chore: add movie tab card

---
+++ lib/presentation/journeys/home/movie_tabbed/movie_tab_card_widget.dart
@@ -0,0 +1,52 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:movie_with_shams/common/constants/size_constants.dart';
+import 'package:movie_with_shams/common/extensions/size_extensions.dart';
+import 'package:movie_with_shams/data/core/api_constants.dart';
+
+class MovieTabCardWidget extends StatelessWidget {
+  final int movieId;
+  final String title;
+  final String posterPath;
+
+  const MovieTabCardWidget({
+    Key? key,
+    required this.movieId,
+    required this.title,
+    required this.posterPath,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {},
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Expanded(
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(
+                Sizes.dimen_16.w.toDouble(),
+              ),
+              child: CachedNetworkImage(
+                imageUrl: '${ApiConstants.BASE_IMAGE_URL}$posterPath',
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+              top: Sizes.dimen_4.h.toDouble(),
+            ),
+            child: Text(
+              title,
+              maxLines: 1,
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.bodyText2,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
