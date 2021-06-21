b67599d2a6e8adf8b72c687d74196cdc8d9a5f5c
Shamim Hossain <hossains159@gmail.com>
2021-06-21 20:06:57 +0600

    chore: add isActive to navigation sub list item

--- lib/presentation/journeys/drawer/navigation_sub_list_item.dart
+++ lib/presentation/journeys/drawer/navigation_sub_list_item.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:movie_with_shams/common/constants/size_constants.dart';
 import 'package:movie_with_shams/common/extensions/size_extensions.dart';
+import 'package:movie_with_shams/presentation/themes/app_text.dart';
 
 class NavigationSubListItem extends StatelessWidget {
   final String title;
   final Function() onPressed;
+  final bool isActive;
 
   const NavigationSubListItem({
     Key? key,
     required this.title,
     required this.onPressed,
+    this.isActive = false,
   }) : super(key: key);
 
   @override
@@ -29,9 +32,13 @@ class NavigationSubListItem extends StatelessWidget {
           contentPadding: EdgeInsets.symmetric(
             horizontal: Sizes.dimen_40.w.toDouble(),
           ),
-          title: Text(
-            title,
-            style: Theme.of(context).textTheme.subtitle1,
+          title: Container(
+            child: Text(
+              title,
+              style: isActive
+                  ? Theme.of(context).textTheme.royalBlueSubtitle1
+                  : Theme.of(context).textTheme.subtitle1,
+            ),
           ),
         ),
       ),
